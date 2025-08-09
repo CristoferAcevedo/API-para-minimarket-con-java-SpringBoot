875c5f5f6d9711755a7b3303035dde38479c656d
CristoferAcevedo <158364057+CristoferAcevedo@users.noreply.github.com>
2025-08-09 15:22:24 -0500

    se le acomodo la clase service a la clase categoria con su respectivo controlador y repositorio

--- src/main/java/com/cristofer/apirest/apirest/Controllers/CategoriaController.java
+++ src/main/java/com/cristofer/apirest/apirest/Controllers/CategoriaController.java
@@ -3,12 +3,16 @@ package com.cristofer.apirest.apirest.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cristofer.apirest.apirest.Entities.Categoria;
-import com.cristofer.apirest.apirest.Repositories.CategoriaRepository;
+import com.cristofer.apirest.apirest.service.CategoriaService;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,16 +21,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class CategoriaController {
 
     @Autowired
-    private CategoriaRepository categoriaRepository;
+    private CategoriaService categoriaService;
 
     @GetMapping
-    public List<Categoria> getCategorias() {
-        return categoriaRepository.findAll();
+    public ResponseEntity<List<Categoria>> obtener_categorias() {
+        List<Categoria> categorias = categoriaService.getCategorias();
+        return ResponseEntity.ok(categorias);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> obtenerCategoriaPorId(@PathVariable Long id) {
+        Categoria categoria = categoriaService.obtenerCategoriaPorId(id);
+        if (categoria != null) {
+            return new ResponseEntity<>(categoria, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping
-    public Categoria crearCategoria(@RequestBody Categoria categoria) {
-        return categoriaRepository.save(categoria);
+    public ResponseEntity<Categoria> crearCategoria(@RequestBody Categoria categoria) {
+        Categoria nuevaCategoria = categoriaService.crearCategoria(categoria);
+        return new ResponseEntity<>(nuevaCategoria, HttpStatus.CREATED);
     }
 
 }
---
+++ src/main/java/com/cristofer/apirest/apirest/service/CategoriaService.java
@@ -0,0 +1,31 @@
+package com.cristofer.apirest.apirest.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.cristofer.apirest.apirest.Entities.Categoria;
+import com.cristofer.apirest.apirest.Repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public List<Categoria> getCategorias() {
+        return categoriaRepository.findAll();
+    }
+
+    public Categoria crearCategoria(@RequestBody Categoria categoria) {
+        return categoriaRepository.save(categoria);
+    }
+
+    public Categoria obtenerCategoriaPorId(Long id) {
+        return categoriaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Categoría no encontrada con id: " + id));
+    }
+
+}
